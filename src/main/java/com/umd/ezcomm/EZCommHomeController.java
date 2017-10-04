2c1d5ec608b5f7a05b1fffec569e39bf31cd5fb5
Ribe Ninan <ribeninan@gmail.com>
2017-10-03 20:50:57 -0400

    Hello World

---
+++ src/main/java/com/umd/ezcomm/EZCommHomeController.java
@@ -0,0 +1,23 @@
+package com.umd.ezcomm;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class EZCommHomeController {
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 * 
+	 * @return
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String homePage(HttpServletRequest request, HttpServletResponse response) {
+
+		return "home";
+	}
+}
